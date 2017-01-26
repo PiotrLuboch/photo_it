88dfa8f5a8e15632d7035d92b8a181171f3a5186
Michal Mosion <mosion93@gmail.com>
2017-01-26 13:18:20 +0100

    grayscale, faster processing

--- app/src/main/java/com/example/michal/photoit/DisplayImage.java
+++ app/src/main/java/com/example/michal/photoit/DisplayImage.java
@@ -5,11 +5,18 @@ import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.michal.photoit.filtering.GrayFilter;
+import com.example.michal.photoit.filtering.NegativeFilter;
+import com.example.michal.photoit.filtering.SepiaFilter;
 
 public class DisplayImage extends AppCompatActivity {
 
     private byte[] bytes;
+    ImageView view;
 
     public DisplayImage(){}
 
@@ -23,9 +30,39 @@ public class DisplayImage extends AppCompatActivity {
         Bundle b = getIntent().getExtras();
         this.bytes = b.getByteArray("image");
 
-        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-        ImageView view = (ImageView) findViewById(R.id.imageView);
+        final Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+         view = (ImageView) findViewById(R.id.imageView);
+       // NegativeFilter neg = new NegativeFilter();
+        //bitmap=neg.filter(bitmap);
         view.setImageBitmap(bitmap);
+        ImageButton negative = (ImageButton) findViewById(R.id.negative);
+        ImageButton gray = (ImageButton) findViewById(R.id.gray);
+        ImageButton sepia = (ImageButton) findViewById(R.id.sepia);
+
+        negative.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NegativeFilter neg = new NegativeFilter();
+                view.setImageBitmap(neg.filter(bitmap));
+
+            }
+        });
+        sepia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SepiaFilter sep = new SepiaFilter();
+                view.setImageBitmap(sep.filter(bitmap));
+
+            }
+        });
+        gray.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GrayFilter gray = new GrayFilter();
+                view.setImageBitmap(gray.filter(bitmap));
+
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/michal/photoit/filtering/GrayFilter.java
+++ app/src/main/java/com/example/michal/photoit/filtering/GrayFilter.java
@@ -3,26 +3,26 @@ package com.example.michal.photoit.filtering;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 
-
 /**
- * Created by piotr on 18.12.16.
+ * Created by Michal on 26.01.2017.
  */
 
-public class NegativeFilter implements Filter {
+public class GrayFilter implements Filter {
     @Override
     public Bitmap filter(Bitmap src) {
-        // create new bitmap with the same settings as source bitmap
         Bitmap bmOut = Bitmap.createBitmap(src.getWidth(), src.getHeight(), src.getConfig());
         // color info
         int A, R, G, B;
+        final double GS_RED = 0.299;
+        final double GS_GREEN = 0.587;
+        final double GS_BLUE = 0.114;
         int pixelColor;
         // image size
         int height = src.getHeight();
         int width = src.getWidth();
         int[] pixels = new int[width * height];
         src.getPixels(pixels, 0, width, 0, 0, width, height);
-        int index = 0;
-        // scan through every pixel
+        int index=0;
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
@@ -31,17 +31,15 @@ public class NegativeFilter implements Filter {
                 // get one pixel
                 pixelColor = src.getPixel(x, y);
                 // saving alpha channel
-                A = Color.alpha(pixelColor);
-                // inverting byte for each R/G/B channel
-                R = 255 - Color.red(pixelColor);
-                G = 255 - Color.green(pixelColor);
-                B = 255 - Color.blue(pixelColor);
-                // set newly-inverted pixel to output image
+                A = Color.alpha(pixels[index]);
+                R = Color.red(pixels[index]);
+                G = Color.green(pixels[index]);
+                B = Color.blue(pixels[index]);
+                R = G = B = (int)(GS_RED * R + GS_GREEN * G + GS_BLUE * B);
                 pixels[index] = Color.argb(A, R, G, B);
             }
         }
         bmOut.setPixels(pixels, 0, width, 0, 0, width, height);
-        // return final bitmap
         return bmOut;
     }
 }
